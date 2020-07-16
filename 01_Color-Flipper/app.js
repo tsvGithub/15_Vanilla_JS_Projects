638d3d8763af10d853e3a3cff197b8719e19985b
Tatjana Svjatova <tsv.mailbox@gmail.com>
2020-07-16 18:20:34 +0300

    slider revievs

--- 01_Color-Flipper/app.js
+++ 01_Color-Flipper/app.js
@@ -1,4 +1,5 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+//to choose color from array
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025", "pink", "yellow"];
 //button
 const btn = document.getElementById("btn");
 //span
@@ -8,14 +9,15 @@ const color = document.querySelector(".color");
 btn.addEventListener("click", function () {
   //get random number of 'colors' array
   const randomNumber = getRandomNumber();
-  //   console.log(randomNumber);
-
-  //set body's background to random number of 'colors' array
+  console.log(randomNumber);
+  //-------------------------------
+  //set body's background to random  color of 'colors' array
   document.body.style.backgroundColor = colors[randomNumber];
-  //set text (color name/number) in span
+  //set span text (color name & number)
   color.textContent = colors[randomNumber];
 });
+
 function getRandomNumber() {
-  //random number in array 'colors'
+  //random color from array 'colors'
   return Math.floor(Math.random() * colors.length);
 }
